a5e7401e53d812c9322589077bfaac17dacb2d79
ripannugraha-alt <pklripannugraha2025@gmail.com>
2026-01-30 15:36:02 +0700

    chore: update destinasi page header content

---
+++ src/app/destinasi/page.tsx
@@ -0,0 +1,10 @@
+import PageHeaderSection from "@/components/sections/destinasi/page-header"
+
+export default function PaketWisataPage() {
+  return (
+    <>
+      <PageHeaderSection />
+      
+    </>
+  )
+}
