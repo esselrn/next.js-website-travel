0a65a73d8a25a1281fea37b7237def66eea7592f
ripannugraha-alt <pklripannugraha2025@gmail.com>
2026-02-03 12:53:13 +0700

    feat: add newsletter section layout

--- src/app/kontak/page.tsx
+++ src/app/kontak/page.tsx
@@ -1,10 +1,11 @@
 import ContactSection from "@/components/sections/kontak/contact-section"
+import NewsletterSection from "@/components/sections/home/newsletter-section"
 
 export default function AboutPage() {
   return (
     <>
       <ContactSection />
-
+      <NewsletterSection />
     </>
   )
 }
