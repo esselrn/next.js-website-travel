e1eba2aa229e107a05cf62130e610a3623120ced
ripannugraha-alt <pklripannugraha2025@gmail.com>
2026-02-06 16:22:03 +0700

    Fix Blog type and connect BlogSection with service backend Supabase

--- src/components/organisms/blog-section.tsx
+++ src/components/organisms/blog-section.tsx
@@ -1,16 +1,25 @@
-import BlogHeader from "@/components/organisms/blog-header"
-import BlogCard from "@/components/organisms/blog-card"
-import { blogs } from "@/constants/blogs"
+'use client'
+
+import { useEffect, useState } from 'react'
+import BlogHeader from '@/components/organisms/blog-header'
+import BlogCard from '@/components/organisms/blog-card'
+import { getBlogs, type Blog } from '@/services/blogs.service' // <-- tambahkan 'type Blog'
 
 export default function BlogSection() {
+  const [blogs, setBlogs] = useState<Blog[]>([])
+
+  useEffect(() => {
+    getBlogs().then(setBlogs)
+  }, [])
+
   return (
     <section className="py-20">
       <div className="max-w-[1440px] mx-auto px-6">
         <BlogHeader />
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {blogs.map((blog, index) => (
-            <BlogCard key={index} {...blog} />
+          {blogs.map((blog) => (
+            <BlogCard key={blog.id} {...blog} />
           ))}
         </div>
       </div>
